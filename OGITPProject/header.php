aef2aee4ebe446619a9bfd93f5114a36d9888e75
NoOneKnowsWho <matrixstar466@gmail.com>
2025-05-25 15:11:00 +0200

    Add files via upload

---
+++ OGITPProject/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="header.css">
+</head>
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="index.php">
+                <img src="Coffee Webshop.png" alt="logo" width="100px">
+            </a>
+        </div>
+
+    </nav>
+</header>
+
+
+
+        <div>
+            
+        </div>
+        
+            </header>
